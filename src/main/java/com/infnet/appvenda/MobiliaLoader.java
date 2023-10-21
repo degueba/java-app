f7dd2f7f3bba9518deb8763342f90ee88a9ddd8e
Rodrigo Botelho <rbdesigner@hotmail.com>
2023-10-20 21:20:03 -0400

    Mobilia loader added - feature 1

--- src/main/java/com/infnet/appvenda/MobiliaLoader.java
+++ src/main/java/com/infnet/appvenda/MobiliaLoader.java
@@ -4,23 +4,53 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import com.infnet.appvenda.model.domain.Mobilia;
-import com.infnet.appvenda.model.domain.Produto;
+import com.infnet.appvenda.model.service.MobiliaService;
 
 @Component
 public class MobiliaLoader implements ApplicationRunner {
 	
+	@Autowired
+	private MobiliaService mobiliaService;
+
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		
+		FileReader file = new FileReader("files/mobilia.txt");
+		BufferedReader leitura = new BufferedReader(file);
+		
+	
+		String linha = leitura.readLine();
+		
+		String[] campos = null;
+		
+		
+		while(linha != null) {
+			Mobilia mobilia = new Mobilia();
+			
+			campos = linha.split(";"); 
+			mobilia.setNome(campos[0]);
+			mobilia.setCor(campos[3]);
+			
+			mobiliaService.incluir(mobilia);
+			
+			linha = leitura.readLine();
+		}
+		
+		
+		for(Mobilia mobilia: mobiliaService.obterLista()) {
+			System.out.println("[Mobilia]: " + mobilia);
+		}
 		
 		
 		
+		leitura.close();
 	}
 
 }
-
